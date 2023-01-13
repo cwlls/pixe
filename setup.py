dcd54b06e007f245f4d9a1e525a56ec53e16f2cd
Chris Wells <chris@fosforics.com>
2023-01-13 16:51:13 -0500

    Simplified tool structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tomte',
+    version='0.1.0',
+    py_modules=['tomte'],
+    url='https://github.com/fosforics/tomte',
+    license='Apache-2',
+    author='Chris Wells',
+    author_email='chris@fosforics.com',
+    description='A digital house gnome to keep your files neat and tidy',
+    include_package_data=True,
+    install_requires=[
+        'Click>=8.1.3,<8.2',
+        'Pillow>=9.4.0,<9.5',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tomte = tomte:cli',
+        ],
+    },
+)
