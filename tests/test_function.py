ae8eb4bb4d977ee5434a4bb65867a364df1d7576
Chris Wells <chris@fosforics.com>
2023-01-15 00:27:52 -0500

    Perform function testing at CLI level

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,40 +1,69 @@
 import pathlib
+import os
 
 import pytest
+from click.testing import CliRunner
 
 import tomte
 
 
 @pytest.fixture
-def dest_path(tmp_path):
-    return pathlib.Path(tmp_path)
+def runner():
+    return CliRunner()
 
 
 @pytest.fixture
 def src_path():
-    return pathlib.Path('data/img')
+    return pathlib.Path(os.path.dirname(os.path.realpath(__file__))).joinpath('data/img')
 
 
 @pytest.fixture
-def file_list(src_path):
-    file_list = []
-    for img in src_path.glob('*.jpg'):
-        file_list.append(img)
-
-    return file_list
+def src_file(src_path):
+    return src_path.joinpath('chocolate.jpg')
 
 
-def test_parallel_process_files(src_path, dest_path, file_list):
-    tomte.parallel_process_files(file_list, dest_path)
+def test_single_file(runner, src_file, tmp_path):
+    dest_file = tmp_path.joinpath('2021/6/20210628_093121_9c12b09015e8fe1bdd3c9aa765d08c5cdd60a485.jpg')
 
-    assert dest_path.joinpath('1902/2/19020220_000000_9c12b09015e8fe1bdd3c9aa765d08c5cdd60a485.jpg').exists()
-    assert dest_path.joinpath('2021/10/20211013_145024_193618a9c1dad0600f5d571268404d73d5a16173.jpg').exists()
-    assert dest_path.joinpath('2022/9/20220928_020550_141e2b9762f60952aa0510ac9309a4ae6126b817.jpg').exists()
+    results = runner.invoke(tomte.cli, f"--dest {tmp_path} {src_file}")
+
+    assert results.exit_code == 0
+    assert dest_file.exists()
 
 
-def test_serial_process_files(src_path, dest_path, file_list):
-    tomte.serial_process_files(file_list, dest_path)
+def test_single_file_no_exist(runner, tmp_path):
+    results = runner.invoke(tomte.cli, f"--dest {tmp_path} this/file/really/does/not/exist")
 
-    assert dest_path.joinpath('1902/2/19020220_000000_9c12b09015e8fe1bdd3c9aa765d08c5cdd60a485.jpg').exists()
-    assert dest_path.joinpath('2021/10/20211013_145024_193618a9c1dad0600f5d571268404d73d5a16173.jpg').exists()
-    assert dest_path.joinpath('2022/9/20220928_020550_141e2b9762f60952aa0510ac9309a4ae6126b817.jpg').exists()
+    assert results.exit_code == 2
+
+
+def test_single_file_bad(runner, tmp_path):
+    results = runner.invoke(tomte.cli, f"--dest {tmp_path} /dev/zero")
+
+    assert results.exit_code == 2
+
+
+def test_files_parallel(runner, src_path, tmp_path):
+    results = runner.invoke(tomte.cli, f"--dest {tmp_path} {src_path}")
+
+    assert results.exit_code == 0
+    assert tmp_path.joinpath('1902/2/19020220_000000_9c12b09015e8fe1bdd3c9aa765d08c5cdd60a485.jpg').exists()
+    assert tmp_path.joinpath('2021/10/20211013_145024_193618a9c1dad0600f5d571268404d73d5a16173.jpg').exists()
+    assert tmp_path.joinpath('2022/9/20220928_020550_141e2b9762f60952aa0510ac9309a4ae6126b817.jpg').exists()
+    assert not tmp_path.joinpath('2022/3/20220305_225114_93f3c5c0b3d0e23349e238231131b3f297889be4.jpg').exists()
+
+
+def test_files_serial(runner, src_path, tmp_path):
+    results = runner.invoke(tomte.cli, f"--no-parallel --dest {tmp_path} {src_path}")
+
+    assert results.exit_code == 0
+    assert tmp_path.joinpath('1902/2/19020220_000000_9c12b09015e8fe1bdd3c9aa765d08c5cdd60a485.jpg').exists()
+    assert tmp_path.joinpath('2021/10/20211013_145024_193618a9c1dad0600f5d571268404d73d5a16173.jpg').exists()
+    assert tmp_path.joinpath('2022/9/20220928_020550_141e2b9762f60952aa0510ac9309a4ae6126b817.jpg').exists()
+
+
+def test_files_recurse(runner, src_path, tmp_path):
+    results = runner.invoke(tomte.cli, f"--recurse --dest {tmp_path} {src_path}")
+
+    assert results.exit_code == 0
+    assert tmp_path.joinpath('2022/3/20220305_225114_93f3c5c0b3d0e23349e238231131b3f297889be4.jpg').exists()
