f0393cad43c5f3eeef7fc3ffebb2300a1a24c86e
Chris Wells <chris@ithuna.com>
2023-02-06 09:31:47 -0500

    Add option shortcuts

--- tomte.py
+++ tomte.py
@@ -102,8 +102,8 @@ def serial_process_files(file_list: list, dest: str):
 
 @click.command()
 @click.argument("src")
-@click.option("--dest", default=".", help="desired destination")
-@click.option("--recurse/--no-recurse", default=False, help="recurse into subdirs (default: --no-recurse)")
+@click.option("--dest", "-d", default=".", help="desired destination")
+@click.option("--recurse", "-r", is_flag=True, default=False, help="recurse into sub-directories (default: off)")
 @click.option("--parallel/--no-parallel", default=True, help="process files in parallel (default: --parallel)")
 def cli(src: str, dest: str, recurse: bool, parallel: bool):
     file_path = pathlib.Path(src)
