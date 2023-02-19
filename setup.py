409b776814c2a5e412ddd39fc4e0da9d33caf736
Chris Wells <chris@ithuna.com>
2023-02-18 22:56:19 -0500

    Bump version to v0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tomte",
-    version="0.2.0",
+    version="0.5.0",
     py_modules=["tomte"],
     url="https://github.com/ithuna/tomte",
     license="Apache-2",
