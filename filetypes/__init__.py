c37d30ea1a96b3d9b1bb22fbdd665d4a2a8a7ffa
Chris Wells <iroh@apache.org>
2023-11-23 16:13:51 -0500

    add image filetype

---
+++ filetypes/__init__.py
@@ -0,0 +1,52 @@
+#
+# Package for all Pixe filetypes
+# each filetype should use the following pattern to link to the
+# main application:
+#   import filetypes
+#   APP = features.APP
+#
+import datetime
+import pathlib
+
+# This name will be updated once the app has been constructed,
+# but before filetypes are loaded.
+APP = None
+
+
+class PixeFile:
+    """
+    A base class for supported file types in Pixe
+    """
+
+    FILE_EXTENSIONS = []
+    NAME = 'PixeFile'
+    ERROR_DATE = datetime.datetime(1902, 2, 20)
+
+    def __init__(self, path: str):
+        # add FILE_EXTENSIONS to the APP
+        APP.add_extensions(self.FILE_EXTENSIONS, self.create_file)
+
+        self.path = pathlib.Path(path)
+
+    def __repr__(self):
+        return f"{self.NAME}: {self.path}"
+
+    @classmethod
+    def supported_extension(cls, extension: str) -> bool:
+        return extension in cls.FILE_EXTENSIONS
+
+    @classmethod
+    def create_file(cls, path: str):
+        return cls.__init__(cls, path)
+
+    # helper functions
+    @property
+    def create_date(self) -> datetime.datetime:
+        raise NotImplementedError
+
+    @property
+    def checksum(self, block_size: int = 8192) -> str:
+        raise NotImplementedError
+
+    def _set_metadata(self, tag: str, value: str):
+        raise NotImplementedError
---
+++ filetypes/image_file.py
@@ -0,0 +1,69 @@
+import logging
+import datetime
+import hashlib
+import io
+import fnmatch
+
+import PIL.Image
+import piexif
+
+import filetypes
+APP = filetypes.APP
+
+LOGGER = logging.getLogger(__name__)
+
+
+class ImageFile(filetypes.PixeFile):
+    """
+    Image files
+    """
+    NAME = 'ImageFile'
+    FILE_EXTENSIONS = ["jpg", "jpeg"]
+
+    def __init__(self, path: str):
+        super().__init__(path)
+
+    # helpers
+    @property
+    def checksum(self, block_size: int = 8192) -> str:
+        """
+        Create a sha1 checksum of just the image data (no meta/exif).
+
+        :param block_size: the block size to use when chunking up the image data
+        :return: a calculated hex digest
+        """
+        hasher = hashlib.sha1()
+        img_io = io.BytesIO()
+
+        # open the image file and save the image data portion as an io.BytesIO object
+        with PIL.Image.open(self.path) as im:
+            im.save(img_io, im.format)
+
+        # reset the cursor
+        img_io.seek(0)
+
+        # chunk_size at a time, update our hash until complete
+        while chunk := img_io.read(block_size):
+            hasher.update(chunk)
+
+        return hasher.hexdigest()
+
+    @property
+    def create_date(self) -> datetime.datetime:
+        """
+        Extract the file creation date from EXIF information.
+
+        :return: a datetime object representing the creation date of the image
+        """
+        # TODO: use piexif: exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal]
+        with PIL.Image.open(self.path, "r") as im:
+            try:
+                # attempt to extract the creation date from EXIF tag 36867
+                exif = im._getexif()
+                cdate = datetime.datetime.strptime(exif[36867], "%Y:%m:%d %H:%M:%S")
+
+            # the requested tag doesn't exist, use the ERROR_DATE global to signify such
+            except KeyError:
+                cdate = self.ERROR_DATE
+
+            return cdate
