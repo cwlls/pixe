a61b921d94088b645b78d672d0507a4d8cf876d3
Chris Wells <iroh@apache.org>
2023-11-24 22:51:17 -0500

    refactor PixeFile class to be a file type factory

--- filetypes/__init__.py
+++ filetypes/__init__.py
@@ -1,60 +1,69 @@
-#
-# Package for all Pixe filetypes
-# each filetype should use the following pattern to link to the
-# main application:
-#   import filetypes
-#   APP = features.APP
-#
-import datetime
+import typing
 import pathlib
-
-# This name will be updated once the app has been constructed,
-# but before filetypes are loaded.
-APP = None
+import datetime
 
 
 class PixeFile:
-    """
-    A base class for supported file types in Pixe
-    """
 
-    FILE_EXTENSIONS = []
-    NAME = 'PixeFile'
-    ERROR_DATE = datetime.datetime(1902, 2, 20)
+    def __init__(self):
+        self._filetypes = {}
 
-    def __init__(self, path: str):
-        # add FILE_EXTENSIONS to the APP
-        APP.add_extensions(self.FILE_EXTENSIONS, self.create_file)
+    def register_filetype(self, extension: str, creator: typing.Callable):
+        self._filetypes[extension] = creator
 
-        self.path = pathlib.Path(path)
+    def get_file_obj(self, extension: str, path: pathlib.Path) -> typing.Callable:
+        if creator := self._filetypes.get(extension):
+            return creator(path)
+        else:
+            raise ValueError
 
-    def __repr__(self):
-        return f"{self.NAME}: {self.path}"
 
-    @classmethod
-    def supported_extension(cls, extension: str) -> bool:
-        return extension in cls.FILE_EXTENSIONS
+# Using a date that shouldn't appear in our collection, but that also isn't a common default.
+# In this case, Ansel Adams birthday.
+ERROR_DATE = datetime.datetime(1902, 2, 20)
 
-    @classmethod
-    def create_file(cls, path: str):
-        return cls.__init__(cls, path)
+factory = PixeFile()
+from . import image_file
 
-    # helper functions
-    @property
-    def creation_date(self) -> datetime.datetime:
-        raise NotImplementedError
-
-    @property
-    def checksum(self, block_size: int = 8192) -> str:
-        raise NotImplementedError
-
-    @classmethod
-    def add_metadata(cls, file: pathlib.Path, **kwargs):
-        """
-        Add a metadata tag to a given file.
-
-        :param file: the file to be acted upon
-        :param tag: a supported tag name
-        :param value: a string to be written to tag
-        """
-        raise NotImplementedError
+# class PixeFile:
+#     """
+#     A base class for supported file types in Pixe
+#     """
+#
+#     FILE_EXTENSIONS = []
+#     NAME = 'PixeFile'
+#     ERROR_DATE = datetime.datetime(1902, 2, 20)
+#
+#     def __init__(self, path: str):
+#         self.path = pathlib.Path(path)
+#
+#     def __repr__(self):
+#         return f"{self.NAME}: {self.path}"
+#
+#     @classmethod
+#     def supported_extension(cls, extension: str) -> bool:
+#         return extension in cls.FILE_EXTENSIONS
+#
+#     @classmethod
+#     def create_file(cls, path: str):
+#         return cls.__init__(cls, path)
+#
+#     # helper functions
+#     @property
+#     def creation_date(self) -> datetime.datetime:
+#         raise NotImplementedError
+#
+#     @property
+#     def checksum(self, block_size: int = 8192) -> str:
+#         raise NotImplementedError
+#
+#     @classmethod
+#     def add_metadata(cls, file: pathlib.Path, **kwargs):
+#         """
+#         Add a metadata tag to a given file.
+#
+#         :param file: the file to be acted upon
+#         :param tag: a supported tag name
+#         :param value: a string to be written to tag
+#         """
+#         raise NotImplementedError
