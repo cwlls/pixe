568838c49eb49adecd44a0ed168de6fd51310287
Chris Wells <iroh@apache.org>
2024-04-28 20:46:38 -0400

    fix owner exif tag setting in image_file and temp remove copyright

--- src/filetypes/image_file.py
+++ src/filetypes/image_file.py
@@ -5,9 +5,8 @@ import io
 import pathlib
 
 import PIL.Image
-import exiftool
-import piexif
 from pillow_heif import HeifImagePlugin
+import exiftool
 
 import filetypes
 from . import base
@@ -22,8 +21,7 @@ class ImageFile(base.PixeFile):
     """
 
     EXTENSIONS = ["jpg", "jpeg", "heic", "heif"]
-    # ALLOWED_TAGS = ["copyright", "owner"]
-    ALLOWED_TAGS = []
+    ALLOWED_TAGS = ["owner"]
 
     def __init__(self, path: pathlib.Path):
         super().__init__(path)
@@ -64,35 +62,25 @@ class ImageFile(base.PixeFile):
 
         return cdate
 
-    ## TODO: this needs to be rewritten to accomodate filetypes other than jpg.
-    ##  also need to fix the ALLOWED_TAGS constant above when done with rewrite.
     # @property
     # def metadata(self):
     #     return piexif.load(str(self.path))
 
-    # @classmethod
-    # def add_metadata(cls, file: pathlib.Path, **kwargs):
-    #     assert file.suffix.lstrip(".").lower() in cls.EXTENSIONS
-    #     for key in kwargs.keys():
-    #         assert key in cls.ALLOWED_TAGS
+    @classmethod
+    def add_metadata(cls, file: pathlib.Path, **kwargs):
+        for tag in kwargs.keys():
+            assert tag in cls.ALLOWED_TAGS
 
-    #     exif_data = piexif.load(str(file))
+        new_tags = {}
+        if owner := kwargs.get("owner"):
+            new_tags["OwnerName"] = owner
+        # TODO: figure out a way to grab the date of the file and use that for
+        #  the copyright year
+        # if copyright := kwargs.get("copyright"):
+        #     new_tags["Copyright"] = copyright
 
-    #     if "owner" in kwargs and kwargs.get("owner") != "":
-    #         exif_data["Exif"][0xA430] = kwargs.get("owner").encode("ascii")
-    #     if "copyright" in kwargs and kwargs.get("copyright") != "":
-    #         exif_data["0th"][piexif.ImageIFD.Copyright] = kwargs.get(
-    #             "copyright"
-    #         ).encode("ascii")
-
-    #     try:
-    #         exif_bytes = piexif.dump(exif_data)
-    #     except ValueError as e:
-    #         LOGGER.error(f"{e}: {str(file)}")
-    #         del exif_data["Exif"][41729]
-    #         exif_bytes = piexif.dump(exif_data)
-    #     finally:
-    #         piexif.insert(exif_bytes, str(file))
+        with exiftool.ExifToolHelper() as et:
+            et.set_tags(file, tags=new_tags, params=["-P", "-overwrite_original"])
 
 
 # add ImageFile extensions and creator method to the Factory
